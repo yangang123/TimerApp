88dd9036e9f0f7b1f271ce4b15862862a93b8c89
yangang <320054593@qq.com>
2020-02-02 13:41:50 +0800

    1. 支持倒计时功能 2. 支持2种设置时间的方法： 手动输入时间和选择时间

--- TimerTest1/Form1.Designer.cs
+++ TimerTest1/Form1.Designer.cs
@@ -29,12 +29,149 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(307, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始定时";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(128, 34);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(131, 23);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "选择时间";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "剩余时间";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(128, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(265, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "秒";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(34, 152);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(384, 30);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(128, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(131, 25);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(265, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "秒";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(31, 78);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "设置时间";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(526, 245);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "倒计时软件";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- TimerTest1/Form1.cs
+++ TimerTest1/Form1.cs
@@ -10,10 +10,126 @@ using System.Windows.Forms;
 namespace TimerTest1
 {
     public partial class Form1 : Form
-    {
+    {   
+        // 倒计时时间
+        int countSet = 0;
+        int countIndex = 0;
+
+        bool countStart = false;
+
+        //选择时间
+        int selectCount = 0;
+
+        // 文本输入
+        int textCount = 0;
+        bool textChange = false;
+
         public Form1()
         {
             InitializeComponent();
+            
+            //添加倒计时时间从1秒 ~ 50秒 
+            int i = 0;
+            for (i = 1; i <= 50; i++)
+            {
+                comboBox1.Items.Add(Convert.ToString(i, 10) + " 秒");
+            }
+
+           
+            //默认选择第一个索引
+            comboBox1.SelectedIndex = 0;    
+         }
+
+        private  void stop()
+        {
+            label3.Text = "0";
+            countStart = false;
+            textChange = false;
+            button1.Text = "开始计时";
+            System.Media.SystemSounds.Hand.Play();
+            MessageBox.Show("倒计时间到");
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {   
+            if (!countStart)
+            {
+                //获取定时时间
+                string secCountStr = comboBox1.SelectedItem.ToString();
+                string secCount = secCountStr.Substring(0, 2);
+
+                //设置定时时间到全局变量countSet,并启动定时器
+                selectCount = Convert.ToUInt16(secCount);
+                if (textChange)
+                {
+                    textCount = Convert.ToUInt16(textBox1.Text);
+                    countSet = textCount;
+                } 
+                else
+                {
+                    selectCount = Convert.ToUInt16(secCount);
+                    countSet = selectCount;
+                }
+                
+                countIndex = 0;
+                progressBar1.Maximum = countSet;
+                label3.Text = countSet.ToString();
+                timer1.Start();
+                countStart = true;
+                button1.Text = "停止计时";
+            }
+            else
+            {
+                //倒计时时间倒，并关闭定时器
+                timer1.Stop();
+
+                //倒计时时间显示最大
+                progressBar1.Value = 0;
+
+                //剩余倒计时时间为0
+                label3.Text = "0";
+                countStart = false;
+                textChange = false;
+                button1.Text = "开始计时";
+                MessageBox.Show("倒计时取消！！");
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (++countIndex == countSet)
+            {
+                //倒计时时间倒，并关闭定时器
+                timer1.Stop();
+
+                //倒计时时间显示最大
+                progressBar1.Value = countIndex;
+
+                //剩余倒计时时间为0
+                stop();
+            }
+            else
+            {  
+                //将倒计时时间显示到进度条中
+                progressBar1.Value = countIndex;
+                int count = countSet - countIndex;
+
+                //将倒计时剩余时间显示倒倒计时窗口中
+                label3.Text = count.ToString();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textChange = true;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
